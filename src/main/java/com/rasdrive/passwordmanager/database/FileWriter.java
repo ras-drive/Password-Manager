10b6a78c1ee5624ebcc60771f29f323e6d9ef464
Sarah <82052490+ras-drive@users.noreply.github.com>
2021-11-19 12:38:33 -0600

    Overloaded a method for later use in the FileWriter class.

--- src/main/java/com/rasdrive/passwordmanager/database/FileWriter.java
+++ src/main/java/com/rasdrive/passwordmanager/database/FileWriter.java
@@ -4,9 +4,18 @@ import javafx.collections.ObservableList;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.LinkdList;
 
 public class FileWriter {
     public static void writeToFile(String fileName, ArrayList<LogIn> logIns) throws IOException {
+        FileOutputStream fileOut = new FileOutputStream(fileName);
+        ObjectOutputStream outputStream = new ObjectOutputStream(fileOut);
+        for (LogIn log : logIns) {
+            outputStream.writeObject(log);
+        }
+    }
+
+    public static void writeToFile(String fileName, LinkedList<Login> logins) throws IOException {
         FileOutputStream fileOut = new FileOutputStream(fileName);
         ObjectOutputStream outputStream = new ObjectOutputStream(fileOut);
         for (LogIn log : logIns) {
